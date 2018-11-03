4b444004eaa82791132fcfe42d1fb968860a1c49
max-96 <massimo.spaconi@gmail.com>
2018-11-03 12:12:25 +0100

    Refactoring

--- Engine/src/util/CardsUtils.java
+++ Engine/src/util/CardsUtils.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public class CardsUtils
 {
-	public static final boolean STATS_COLLECT = false; //TODO Questo e' un setting
+	public static final boolean STATS_COLLECT = false; // TODO Questo e' un setting
 
-	public final static double[] puntiPerCarta = { 1, 1.0 / 3, 1.0 / 3, 0, 0, 0, 0, 1.0 / 3, 1.0 / 3, 1.0 / 3 };
-	public final static int[] dominioPerCarta = { 7, 8, 9, 0, 1, 2, 3, 4, 5, 6 };
+	private final static double[] puntiPerCarta = { 1, 1.0 / 3, 1.0 / 3, 0, 0, 0, 0, 1.0 / 3, 1.0 / 3, 1.0 / 3 };
+	private final static int[] dominioPerCarta = { 7, 8, 9, 0, 1, 2, 3, 4, 5, 6 };
 
 	public static final int QUATTRODIDENARI = 3;
 
@@ -43,32 +43,32 @@ public class CardsUtils
 	private static double findBongioco(List<Integer> carteInMano, HashSet<Integer> accusi)
 	{
 		double points = 0;
-		int[] accusoCards = {0, 0, 0};
-		
+		int[] accusoCards = { 0, 0, 0 };
+
 		for (int c : carteInMano)
 			if (c % 10 < 3)
 				accusoCards[c % 10]++;
-		
+
 		for (int i = 0; i < 3; i++)
 			if (accusoCards[i] > 2)
 			{
 				for (int c : carteInMano)
 					if (c % 10 == i)
 						accusi.add(c);
-				
+
 				points += accusoCards[i];
 			}
-			
+
 		return points;
 	}
 
 	private static double findNapoli(List<Integer> carteInMano, HashSet<Integer> accusi)
 	{
 		double points = 0;
-		int[] accusoCards = {0, 0, 0, 0};
-		
+		int[] accusoCards = { 0, 0, 0, 0 };
+
 		for (int c : carteInMano)
-			if(c % 10 < 3)
+			if (c % 10 < 3)
 				accusoCards[c / 10]++;
 
 		for (int i = 0; i < 4; i++)
@@ -77,10 +77,10 @@ public class CardsUtils
 				accusi.add(i * 10);
 				accusi.add(i * 10 + 1);
 				accusi.add(i * 10 + 2);
-				
+
 				points += 3;
 			}
-		
+
 		return points;
 	}
 
@@ -88,34 +88,43 @@ public class CardsUtils
 	{
 		return card / 10;
 	}
-	
+
 	public static double getCardPoints(int card)
 	{
-		return puntiPerCarta[card%10];
+		return puntiPerCarta[card % 10];
 	}
-	
+
 	public static int getCardDominance(int card)
 	{
-		return dominioPerCarta[card%10];
+		return dominioPerCarta[card % 10];
 	}
+
 	public static boolean isSameSuit(int card1, int card2)
 	{
-		return card1/10 == card2/10;
+		return card1 / 10 == card2 / 10;
 	}
-//	public static int getDominantCard(List<Integer> cards)
-//	{
-//		assert cards.size() > 0;
-//		int d = cards.get(0);
-//		for (int i = 1; i < cards.size(); i++)
-//		{
-//			int s = cards.get(i);
-//			if (d / 10 != s / 10)
-//				continue;
-//			if (dominioPerCarta[s % 10] > dominioPerCarta[d % 10])
-//				d = s;
-//		}
-//		return d;
-//	}
+
+	public static double getPointsOfCards(List<Integer> l)
+	{
+		double s = 0;
+		for (int i : l)
+			s += puntiPerCarta[i % 10];
+		return s;
+	}
+	// public static int getDominantCard(List<Integer> cards)
+	// {
+	// assert cards.size() > 0;
+	// int d = cards.get(0);
+	// for (int i = 1; i < cards.size(); i++)
+	// {
+	// int s = cards.get(i);
+	// if (d / 10 != s / 10)
+	// continue;
+	// if (dominioPerCarta[s % 10] > dominioPerCarta[d % 10])
+	// d = s;
+	// }
+	// return d;
+	// }
 
 	public static int getDominantPlayer(List<Integer> cards, int startPlayer)
 	{
@@ -131,15 +140,15 @@ public class CardsUtils
 		}
 		return d;
 	}
-	
+
 	public static List<Integer> getPossibiliMosse(List<Integer> carteInMano, List<Integer> cardsOnTable)
 	{
 		if (cardsOnTable.isEmpty())
 			return new ArrayList<>(carteInMano);
-		
+
 		List<Integer> mosse = new ArrayList<>();
 		int semeAttuale = cardsOnTable.get(0) / 10;
-		
+
 		for (Integer c : carteInMano)
 			if (c / 10 == semeAttuale)
 				mosse.add(c);
@@ -152,8 +161,8 @@ public class CardsUtils
 
 	/**
 	 * 
-	 * It sorts the moves by how much they restrict the next player's moves.
-	 * (Lowest Branching Factor Heuristic)
+	 * It sorts the moves by how much they restrict the next player's moves. (Lowest
+	 * Branching Factor Heuristic)
 	 * 
 	 * @param onTable
 	 *            cards on the table
@@ -197,13 +206,13 @@ public class CardsUtils
 		}
 
 		Comparator<Integer> t = new Comparator<Integer>()
+		{
+			@Override
+			public int compare(Integer arg0, Integer arg1)
 			{
-				@Override
-				public int compare(Integer arg0, Integer arg1)
-				{
-					return (int) ((score[arg0.intValue() / 10] - score[arg1.intValue() / 10]) * 5);
-				}
-			};
+				return (int) ((score[arg0.intValue() / 10] - score[arg1.intValue() / 10]) * 5);
+			}
+		};
 
 		Collections.sort(mosse, t);
 		return true;
